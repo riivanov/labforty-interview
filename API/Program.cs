3f91828599603273b1f639cd248881ef76b88a0f
Radoslav Ivanov <riivanov63@gmail.com>
2023-12-25 01:13:16 +0200

    add JWT DI services - RUNTIME ERROR

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,41 @@
+using System.Text;
 using System.Text.Json.Serialization;
+using API.Auth;
 using API.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder
+    .Services.AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(o =>
+    {
+        var Key = Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]);
+        o.SaveToken = true;
+        o.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["JWT:Issuer"],
+            ValidAudience = builder.Configuration["JWT:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Key)
+        };
+    });
+
+builder
+    .Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<IdentityDbContext>()
+    .AddDefaultTokenProviders();
 
 // Add services to the container.
 
@@ -12,9 +45,21 @@ builder
         opt => opt.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles
     );
 
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.ExpireTimeSpan = TimeSpan.FromHours(4);
+    options.SlidingExpiration = true;
+    options.LoginPath = new PathString("/Account");
+    options.ReturnUrlParameter = "returnURL";
+    //other properties
+});
+
 builder.Services.AddDbContext<APIDbContext>(
     option => option.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection"))
 );
+
+builder.Services.AddTransient<IdentityDbContext, LoginContext>();
+builder.Services.AddSingleton(builder.Configuration);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -27,12 +72,23 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    DBInitializer.CreateDbIfNotExists(app);
 }
 
+// app.UseMiddleware<ApiKeyMiddleware>();
+
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapControllerRoute(
+    name: "Identity",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+);
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
