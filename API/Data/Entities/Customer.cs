cd58ebf230458ddc79a90b559783473f4e3feb7b
Radoslav Ivanov <riivanov63@gmail.com>
2023-12-24 22:14:39 +0200

    CRUD works; next - validation

--- API/Data/Entities/Customer.cs
+++ API/Data/Entities/Customer.cs
@@ -8,6 +8,8 @@ public class Customer
     public int Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
+
+    // TODO(rivanov): validate age (18-100)
     public byte Age { get; set; }
 
     public ICollection<Order> Orders { get; set; }
--- API/Data/Entities/Product.cs
+++ API/Data/Entities/Product.cs
@@ -5,6 +5,7 @@ namespace API.Data.Entities;
 public class Product
 {
     [Key]
+    // TODO(rivanov): post.json needs increment before each post; doesn't autoincrement
     public string Id { get; set; }
     public string Name { get; set; }
     public decimal Price { get; set; }
